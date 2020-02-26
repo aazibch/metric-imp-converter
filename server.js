21ea4fce8cf79c723efc61039a6af45e538d458a
Aazib Chaudhry <aazibch47@gmail.com>
2020-02-26 08:16:13 +0500

    Add a handler function for "uncaughtException"

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 require('dotenv').config();
+
+process.on('uncaughtException', (err) => {
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 const app = require('./app');
 
 const port = process.env.PORT || 5000;
