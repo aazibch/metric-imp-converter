28a52e04c3f63933b0c9782496156fbd989e957f
Aazib Chaudhry <aazibch47@gmail.com>
2021-01-06 14:19:55 +0500

    Add functionality to respond to the "unhandledRejection" event

--- server.js
+++ server.js
@@ -11,3 +11,10 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    }); 
+});
